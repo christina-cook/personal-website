0c7896fa4af90c550ce26bb498f7407b48428f26
Christina Cook <cmc.cook7@gmail.com>
2021-06-01 11:06:55 -0600

    Add arc to home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import './Home.css';
+import arc from '../../assets/arc.png';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <section className='home-section'>
       <div className='welcome'>
+        <img src={arc} alt='software enginer' className='arc'/>
         <h1 className='home-title'>Hi! I'm <span className='first-name'>Christina</span></h1>
       </div>
       <p className='intro'>A software engineer based in Denver, Colorado.</p>
