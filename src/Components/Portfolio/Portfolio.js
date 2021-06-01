2622d591b5c543a3c0ad99b5a1da4b1f1e7177ae
Christina Cook <cmc.cook7@gmail.com>
2021-06-01 13:49:01 -0600

    Display projects on portfolio page

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,9 +1,38 @@
+import { useState, useEffect } from 'react';
 import './Portfolio.css';
+import ProjectCard from '../ProjectCard/ProjectCard';
+import projectsData from '../../data/projects';
 
 const Portfolio = () => {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    setProjects(projectsData)
+    console.log('projectsData', projectsData)
+  }, [])
+
+  const projectsToDisplay = () => {
+    return projects.map(project => {
+      return (
+        <ProjectCard
+          key={project.id}
+          title={project.title}
+          image={project.image}
+          stack={project.stack.join(', ')}
+          description={project.description}
+          app={project.app}
+          repo={project.repo}
+        />
+      )
+    })
+  }
+
   return (
     <section className='portfolio'>
-      <h2>Portfolio</h2>
+      <h2 className='portfolio-title'>My Projects</h2>
+      <div className='project-grid'>
+        {projectsToDisplay()}
+      </div>
     </section>
   )
 }
