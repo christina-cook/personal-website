7c2ca1e6186124052d622f616da9e5cc3f0e4a30
Christina Cook <cmc.cook7@gmail.com>
2021-06-01 10:54:18 -0600

    Add logo to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,12 @@
 import './Header.css';
+import logo from '../../assets/logo.png';
+
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
+      <img src={logo} alt='coded-by-christina' className='logo'/>
       <nav className='navbar'>
         <ul className='nav-list'>
           <li className='list-item'>
