b6966e809f234e89f2ab0d9abfb2f1e5508dda70
Christina Cook <cmc.cook7@gmail.com>
2021-06-01 10:52:33 -0600

    Add navlinks to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,49 @@
 import './Header.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
-      <h1>Coded by Christina</h1>
+      <nav className='navbar'>
+        <ul className='nav-list'>
+          <li className='list-item'>
+            <NavLink
+              exact to='/'
+              className='navlink'
+              activeStyle={{
+                color: '#ACCBC5',
+                borderBottom: '2px solid #ACCBC5'
+              }}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className='list-item'>
+            <NavLink
+              to='/portfolio'
+              className='navlink'
+              activeStyle={{
+                color: '#ACCBC5',
+                borderBottom: '2px solid #ACCBC5'
+              }}
+            >
+              Portfolio
+            </NavLink>
+          </li>
+          <li className='list-item'>
+            <NavLink
+              to='/contact'
+              className='navlink'
+              activeStyle={{
+                color: '#ACCBC5',
+                borderBottom: '2px solid #ACCBC5'
+              }}
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
