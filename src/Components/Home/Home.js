ece0e4376ea8abcff05173fcbcd30f0551879d5f
Christina Cook <cmc.cook7@gmail.com>
2021-06-01 11:05:15 -0600

    Add text to home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,20 @@
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <section className='home-section'>
-      <h2>Home</h2>
+      <div className='welcome'>
+        <h1 className='home-title'>Hi! I'm <span className='first-name'>Christina</span></h1>
+      </div>
+      <p className='intro'>A software engineer based in Denver, Colorado.</p>
+      <Link
+        to='/portfolio'
+      >
+        <button className='work-button'>
+          MY WORK
+        </button>
+      </Link>
     </section>
   )
 }
